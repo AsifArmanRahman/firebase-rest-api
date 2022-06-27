cc461d2bab23d295b46daf2ef6c925c90bbcd84c
Asif Arman Rahman <asifarmanrahman@gmail.com>
2022-06-27 08:52:02 +0600

    test: add config.py

---
+++ tests/config.py
@@ -0,0 +1,36 @@
+
+#   Copyright (c) 2022 Asif Arman Rahman
+#   Licensed under MIT (https://github.com/AsifArmanRahman/firebase/blob/main/LICENSE)
+
+# --------------------------------------------------------------------------------------
+
+
+from decouple import config
+
+
+# get this from firebase console
+# go to project settings, general tab and click "Add Firebase to your web app"
+SIMPLE_CONFIG = {
+    "apiKey": config('FIREBASE_API_KEY'),
+    "authDomain": config('FIREBASE_AUTH_DOMAIN'),
+    "databaseURL": config('FIREBASE_DATABASE_URL'),
+    "storageBucket": config('FIREBASE_STORAGE_BUCKET'),
+}
+
+# get this in json file from firebase console
+# go to project settings, service accounts tab and click generate new private key
+SERVICE_ACCOUNT = {
+    "type": 'service_account',
+    "project_id": config('FIREBASE_SERVICE_ACCOUNT_PROJECT_ID'),
+    "private_key_id": config('FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY_ID'),
+    "private_key": config('FIREBASE_SERVICE_ACCOUNT_PRIVATE_KEY').replace('\\n', '\n'),
+    "client_email": config('FIREBASE_SERVICE_ACCOUNT_CLIENT_EMAIL'),
+    "client_id": config('FIREBASE_SERVICE_ACCOUNT_CLIENT_ID'),
+    "auth_uri": 'https://accounts.google.com/o/oauth2/auth',
+    "token_uri": 'https://oauth2.googleapis.com/token',
+    "auth_provider_x509_cert_url": 'https://www.googleapis.com/oauth2/v1/certs',
+    "client_x509_cert_url": config('FIREBASE_SERVICE_ACCOUNT_CLIENT_X509_CERT_URL'),
+}
+
+
+SERVICE_CONFIG = dict(SIMPLE_CONFIG, serviceAccount=SERVICE_ACCOUNT)
