5e6b7f92cd442fae62385f5d9bb22ee3b4720e77
Asif Arman Rahman <asifarmanrahman@gmail.com>
2022-07-07 11:35:53 +0600

    docs: add documentation for 𝙁𝙞𝙧𝙚𝙗𝙖𝙨𝙚 class

--- firebase/__init__.py
+++ firebase/__init__.py
@@ -5,6 +5,11 @@
 # --------------------------------------------------------------------------------------
 
 
+"""A simple python wrapper for Google's `Firebase`_ REST APIs.
+
+.. _Firebase: https://firebase.google.com/
+"""
+
 from .auth import Auth
 from .storage import Storage
 from .database import Database
@@ -13,13 +18,28 @@ from ._service_account_credentials import _service_account_creds_from_secret
 
 
 def initialize_app(config):
+	"""Initializes and returns a new Firebase instance.
+
+	:type config: dict
+	:param config: Firebase configuration
+
+	:return: A newly initialized instance of Firebase.
+	:rtype: Firebase
+	"""
+
 	return Firebase(config)
 
 
 class Firebase:
-	""" Firebase Interface """
+	""" Firebase Interface
+
+	:type config: dict
+	:param config: Firebase configuration
+	"""
 
 	def __init__(self, config):
+		""" Constructor """
+
 		self.api_key = config["apiKey"]
 		self.auth_domain = config["authDomain"]
 		self.database_url = config["databaseURL"]
@@ -32,10 +52,30 @@ class Firebase:
 			self.credentials = _service_account_creds_from_secret(config['serviceAccount'])
 
 	def auth(self):
+		"""Initializes and returns a new Firebase Authentication
+		instance.
+
+		:return: A newly initialized instance of Auth.
+		:rtype: Auth
+		"""
+
 		return Auth(self.api_key, self.credentials, self.requests)
 
 	def database(self):
+		"""Initializes and returns a new Firebase Realtime Database
+		instance.
+
+		:return: A newly initialized instance of Database.
+		:rtype: Database
+		"""
+
 		return Database(self.api_key, self.credentials, self.database_url, self.requests)
 
 	def storage(self):
+		"""Initializes and returns a new Firebase Storage instance.
+
+		:return: A newly initialized instance of Storage.
+		:rtype: Storage
+		"""
+
 		return Storage(self.credentials, self.requests, self.storage_bucket)
