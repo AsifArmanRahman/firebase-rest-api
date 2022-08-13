2aa9916d5d7c19e03d1e84d0309245cecc8e5bcb
Asif Arman Rahman <asifarmanrahman@gmail.com>
2022-08-13 20:58:52 +0600

    test(firestore): cases for add() method

--- tests/test_firestore.py
+++ tests/test_firestore.py
@@ -6,7 +6,7 @@
 
 
 class TestFirestoreAdmin:
-	movies = {
+	movies1 = {
 		'name': 'Iron Man',
 		'lead': {'name': 'Robert Downey Jr.'},
 		'released': False,
@@ -16,29 +16,53 @@ class TestFirestoreAdmin:
 		'cast': ['Jon Favreau', 'Gwyneth Paltrow', 'Jeff Bridges', b'J.A.R.V.I.S', 'Terrence Howard']
 	}
 
+	movies2 = {
+		'name': 'Thor',
+		'lead': {'name': 'Chris Hemsworth'},
+		'released': False,
+		'year': 2011,
+		'rating': 7.0,
+		'cast': ['Tom Hiddleston', 'Natalie Portman', 'Anthony Hopkins', 'Jeremy Renner', 'Stellan Skarsgård', 'Idris Elba', 'Kat Dennings']
+	}
+
+	auto_doc_id = None
+
 	def test_manual_doc_set(self, ds_admin):
-		assert ds_admin.collection('Marvels').document('Movies').set(self.__class__.movies) is None
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').set(self.__class__.movies1) is None
+
+	def test_auto_doc_add(self, ds_admin):
+		doc_id = ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').add(self.__class__.movies2)
+		assert doc_id
+
+		self.__class__.auto_doc_id = doc_id
 
 	def test_manual_doc_get(self, ds_admin):
-		assert ds_admin.collection('Marvels').document('Movies').get() == self.__class__.movies
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').get() == self.__class__.movies1
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document(self.__class__.auto_doc_id).get() == self.__class__.movies2
 
 	def test_collection_get(self, ds_admin):
-		assert ds_admin.collection('Marvels').get() == [{'Movies': self.__class__.movies}]
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').get() == [{'001': self.__class__.movies1}, {self.__class__.auto_doc_id: self.__class__.movies2}]
 
 	def test_manual_doc_update(self, ds_admin):
 		update_data = {'released': True}
-		assert ds_admin.collection('Marvels').document('Movies').update(update_data) is None
-		assert ds_admin.collection('Marvels').document('Movies').get(field_paths=['released']) == update_data
+
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').update(update_data) is None
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').get(field_paths=['released']) == update_data
+
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document(self.__class__.auto_doc_id).update(update_data) is None
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document(self.__class__.auto_doc_id).get(field_paths=['released']) == update_data
 
 	def test_manual_doc_get_filtered(self, ds_admin):
-		assert ds_admin.collection('Marvels').document('Movies').get(field_paths=['name']) == {'name': self.__class__.movies['name']}
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').get(field_paths=['name']) == {'name': self.__class__.movies1['name']}
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document(self.__class__.auto_doc_id).get(field_paths=['name']) == {'name': self.__class__.movies2['name']}
 
 	def test_manual_doc_delete(self, ds_admin):
-		assert ds_admin.collection('Marvels').document('Movies').delete() is None
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document('001').delete() is None
+		assert ds_admin.collection('Marvels').document('Movies').collection('PhaseOne').document(self.__class__.auto_doc_id).delete() is None
 
 
 class TestFirestoreAuth:
-	movies = {
+	movies1 = {
 		'name': 'Dr. Strange',
 		'lead': {'name': 'Benedict Cumberbatch'},
 		'released': False,
@@ -47,7 +71,19 @@ class TestFirestoreAuth:
 		'prequel': None,
 		'cast': ['Tilda Swinton', 'Rachel McAdams', 'Mads Mikkelsen', 'Chiwetel Ejiofor', 'Benedict Wong']
 	}
+
+	movies2 = {
+		'name': 'Black Panther',
+		'lead': {'name': 'Chadwick Boseman'},
+		'released': False,
+		'year': 2018,
+		'rating': 7.3,
+		'prequel': None,
+		'cast': ['Michael B. Jordan', 'Sebastian Stan', 'Letitia Wright', 'Martin Freeman', 'Winston Duke']
+	}
+
 	user = None
+	auto_doc_id = None
 
 	def test_create_test_user(self, auth):
 		user = auth.sign_in_anonymous()
@@ -56,31 +92,54 @@ class TestFirestoreAuth:
 		assert user.get('idToken')
 
 	def test_manual_doc_set(self, ds):
-		assert ds.collection('Marvels').document('Movies').set(self.__class__.movies, token=self.__class__.user.get('idToken')) is None
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').set(self.__class__.movies1, token=self.__class__.user.get('idToken')) is None
+
+	def test_auto_doc_add(self, ds):
+		doc_id = ds.collection('Marvels').document('Movies').collection('PhaseThree').add(self.__class__.movies2, token=self.__class__.user.get('idToken'))
+		assert doc_id
+
+		self.__class__.auto_doc_id = doc_id
 
 	def test_manual_doc_get(self, ds):
-		assert ds.collection('Marvels').document('Movies').get(token=self.__class__.user.get('idToken')) == self.__class__.movies
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').get(token=self.__class__.user.get('idToken')) == self.__class__.movies1
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document(self.__class__.auto_doc_id).get(token=self.__class__.user.get('idToken')) == self.__class__.movies2
 
 	def test_collection_get(self, ds):
-		assert ds.collection('Marvels').get(token=self.__class__.user.get('idToken')) == [{'Movies': self.__class__.movies}]
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').get(token=self.__class__.user.get('idToken')) == [{'014': self.__class__.movies1}, {self.__class__.auto_doc_id: self.__class__.movies2}]
 
 	def test_manual_doc_get_filtered(self, ds):
-		assert ds.collection('Marvels').document('Movies').get(field_paths=['name'], token=self.__class__.user.get('idToken')) == {'name': self.__class__.movies['name']}
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').get(field_paths=['name'], token=self.__class__.user.get('idToken')) == {'name': self.__class__.movies1['name']}
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document(self.__class__.auto_doc_id).get(field_paths=['name'], token=self.__class__.user.get('idToken')) == {'name': self.__class__.movies2['name']}
 
 	def test_manual_doc_update(self, ds):
 		update_data = {'released': True}
-		assert ds.collection('Marvels').document('Movies').update(update_data, token=self.__class__.user.get('idToken')) is None
-		assert ds.collection('Marvels').document('Movies').get(field_paths=['released'], token=self.__class__.user.get('idToken')) == update_data
+
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').update(update_data, token=self.__class__.user.get('idToken')) is None
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').get(field_paths=['released'], token=self.__class__.user.get('idToken')) == update_data
+
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document(self.__class__.auto_doc_id).update(update_data, token=self.__class__.user.get('idToken')) is None
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document(self.__class__.auto_doc_id).get(field_paths=['released'], token=self.__class__.user.get('idToken')) == update_data
 
 	def test_manual_doc_delete(self, ds):
-		assert ds.collection('Marvels').document('Movies').delete(self.__class__.user.get('idToken')) is None
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document('014').delete(self.__class__.user.get('idToken')) is None
+		assert ds.collection('Marvels').document('Movies').collection('PhaseThree').document(self.__class__.auto_doc_id).delete(self.__class__.user.get('idToken')) is None
 
 	def test_delete_test_user(self, auth):
 		assert auth.delete_user_account(self.__class__.user.get('idToken'))
 
 
 class TestFirestore:
-	series = {
+	series1 = {
+		'name': 'Loki',
+		'lead': {'name': 'Tom Hiddleston'},
+		'released': False,
+		'year': 2021,
+		'rating': 8.2,
+		'prequel': None,
+		'cast': ['Sophia Di Martino', 'Owen Wilson', 'Jonathan Majors', 'Wunmi Mosaku', 'Gugu Mbatha-Raw']
+	}
+	
+	series2 = {
 		'name': 'Moon Knight',
 		'lead': {'name': 'Oscar Issac'},
 		'released': False,
@@ -90,22 +149,37 @@ class TestFirestore:
 		'cast': ['Ethan Hawke', 'May Calamawy', 'F. Murray Abraham']
 	}
 
+	auto_doc_id = None
+
 	def test_manual_doc_set(self, ds):
-		assert ds.collection('Marvels').document('Series').set(self.__class__.series) is None
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').set(self.__class__.series1) is None
+
+	def test_auto_doc_add(self, ds):
+		doc_id = ds.collection('Marvels').document('Series').collection('PhaseFour').add(self.__class__.series2)
+		assert doc_id
+
+		self.__class__.auto_doc_id = doc_id
 
 	def test_manual_doc_get(self, ds):
-		assert ds.collection('Marvels').document('Series').get() == self.__class__.series
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').get() == self.__class__.series1
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document(self.__class__.auto_doc_id).get() == self.__class__.series2
 
 	def test_collection_get(self, ds):
-		assert ds.collection('Marvels').get() == [{'Series': self.__class__.series}]
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').get() == [{'003': self.__class__.series1}, {self.__class__.auto_doc_id: self.__class__.series2}]
 
 	def test_manual_doc_get_filtered(self, ds):
-		assert ds.collection('Marvels').document('Series').get(field_paths=['name']) == {'name': self.__class__.series['name']}
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').get(field_paths=['name']) == {'name': self.__class__.series1['name']}
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document(self.__class__.auto_doc_id).get(field_paths=['name']) == {'name': self.__class__.series2['name']}
 
 	def test_manual_doc_update(self, ds):
 		update_data = {'released': True}
-		assert ds.collection('Marvels').document('Series').update(update_data) is None
-		assert ds.collection('Marvels').document('Series').get(field_paths=['released']) == update_data
+
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').update(update_data) is None
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').get(field_paths=['released']) == update_data
+
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document(self.__class__.auto_doc_id).update(update_data) is None
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document(self.__class__.auto_doc_id).get(field_paths=['released']) == update_data
 
 	def test_manual_doc_delete(self, ds):
-		assert ds.collection('Marvels').document('Series').delete() is None
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document('003').delete() is None
+		assert ds.collection('Marvels').document('Series').collection('PhaseFour').document(self.__class__.auto_doc_id).delete() is None
