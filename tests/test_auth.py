d3bf85002bd99fc5e89cb1033ae183f1ea4a1b77
Asif Arman Rahman <asifarmanrahman@gmail.com>
2023-03-25 00:03:14 +0600

    test(auth): cases for verify_id_token

--- tests/test_auth.py
+++ tests/test_auth.py
@@ -83,6 +83,15 @@ class TestAuth:
 
 		assert "'NoneType' object has no attribute 'valid'" in str(exc_info.value)
 
+	def test_verify_id_token(self, auth):
+		with pytest.raises(KeyError) as exc_info:
+			auth.verify_id_token(self.__class__.user.get('idToken'))['premium'] is True
+		assert "'premium'" in str(exc_info.value)
+
+		with pytest.raises(KeyError) as exc_info:
+			auth.verify_id_token(self.__class__.anonymous_user.get('idToken'))['premium'] is True
+		assert "'premium'" in str(exc_info.value)
+
 	def test_delete_user_account(self, auth):
 		assert auth.delete_user_account(self.__class__.user.get('idToken'))
 		assert auth.delete_user_account(self.__class__.anonymous_user.get('idToken'))
@@ -145,9 +154,6 @@ class TestAuthAdmin:
 		assert user1
 		assert user2
 
-	def test_refresh(self, auth_admin):
-		assert auth_admin.refresh(self.__class__.user.get('refreshToken'))
-
 	def test_get_account_info(self, auth_admin):
 		assert auth_admin.get_account_info(self.__class__.user.get('idToken'))
 
@@ -170,6 +176,20 @@ class TestAuthAdmin:
 	def test_set_custom_user_claims(self, auth_admin):
 		auth_admin.set_custom_user_claims(self.__class__.user.get('localId'), {'premium': True})
 		auth_admin.set_custom_user_claims(self.__class__.anonymous_user.get('localId'), {'premium': True})
+
+	def test_refresh(self, auth_admin):
+		self.__class__.user = auth_admin.refresh(self.__class__.user.get('refreshToken'))
+		self.__class__.custom_user = auth_admin.refresh(self.__class__.custom_user.get('refreshToken'))
+		self.__class__.anonymous_user = auth_admin.refresh(self.__class__.anonymous_user.get('refreshToken'))
+
+	def test_verify_id_token(self, auth_admin):
+		assert auth_admin.verify_id_token(self.__class__.user.get('idToken'))['premium'] is True
+		assert auth_admin.verify_id_token(self.__class__.anonymous_user.get('idToken'))['premium'] is True
+		assert auth_admin.verify_id_token(self.__class__.custom_user_with_claims.get('idToken'))['premium'] is True
+
+		with pytest.raises(KeyError) as exc_info:
+			auth_admin.verify_id_token(self.__class__.custom_user.get('idToken'))['premium'] is True
+		assert "'premium'" in str(exc_info.value)
 
 	def test_delete_user_account(self, auth_admin):
 		assert auth_admin.delete_user_account(self.__class__.user.get('idToken'))
